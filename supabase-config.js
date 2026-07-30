3ca7492b5d85814c9711b8a008c518f033ec399b
damirdauletov29-byte <damirdauletov29@gmail.com>
2026-07-30 22:21:02 +0300

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+// supabase-config.js
+const SUPABASE_URL = 'https://jagngvfawkrglnxuojtq.supabase.co'; // ← ВАШ URL из Project Settings → API
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImphZ25ndmZhd2tyZ2xueHVvanRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU0MzM2MjksImV4cCI6MjEwMTAwOTYyOX0.V42tNuNn1NI6mGMgKRqk3M9gi33dC3IUpDe1M1ORYeM'; // ← ВАШ anon public key
+
+export { SUPABASE_URL, SUPABASE_ANON_KEY };
